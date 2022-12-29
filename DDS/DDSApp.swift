8000208756911fdfdefa12083ba470b4ecb87fa4
Nirmit Dagly <nirmitdagly@gmail.com>
2022-12-30 09:55:11 +1100

    Initial Commit

---
+++ DDS/DDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDSApp.swift
+//  DDS
+//
+//  Created by Nirmit Dagly on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
